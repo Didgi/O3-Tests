2e88a0a5ae9772e65786380e4d682c2dc9601856
sofiyachelpanovaQA <sofiyachelpanova@gmail.com>
2026-09-25 15:17:32 +0300

    Visit and appointment api tests (#20)
    * Исправлены замечания в генерации данных, добавлены аннотации для предшагов

--- src/main/java/api/models/appointment/AppointmentCreateRequest.java
+++ src/main/java/api/models/appointment/AppointmentCreateRequest.java
@@ -1,15 +1,29 @@
 package api.models.appointment;
 
+import api.utils.GeneratingRule;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Setter;
 
+import static api.utils.RegexData.END_DATE;
+import static api.utils.RegexData.START_DATE;
+
+@Builder(toBuilder = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record AppointmentCreateRequest(
+        @GeneratingRule(property = "patient_identifier_type_uuid")
         String patientUuid,
+        @GeneratingRule(property = "test_appointment_service_uuid")
         String serviceUuid,
+        @GeneratingRule(regex = START_DATE)
         String startDateTime,
+        @GeneratingRule(regex = END_DATE)
         String endDateTime,
+        @GeneratingRule(property = "test_appointment_kind")
         String appointmentKind,
+        @GeneratingRule(property = "test_location_uuid")
         String locationUuid,
+        @GeneratingRule(regex = START_DATE)
         String dateAppointmentScheduled
 ) {
 }
